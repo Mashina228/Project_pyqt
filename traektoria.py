0b7b4f24cdb367b2ef160cb1611b06f1997df64f
Rusik <rusgaliul00@gmail.com>
2018-12-09 15:53:00 +0300

    NEW

--- traektoria.py
+++ traektoria.py
@@ -1,37 +1,42 @@
-from PyQt5 import Qt
-import pyqtgraph as pg
 import math
+import sys, random
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtCore import Qt
 
 
-class Window(Qt.QWidget):
+class Example(QWidget):
 
     def __init__(self):
         super().__init__()
 
-        layout = Qt.QVBoxLayout(self)
+        self.initUI()
 
-        self.view = view = pg.PlotWidget()
-        self.curve = view.plot(name="Line")
+    def initUI(self):
+        self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('Points')
+        self.show()
 
-        self.btn = Qt.QPushButton("Random plot")
-        self.btn.clicked.connect(self.random_plot)
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawPoints(qp)
+        qp.end()
 
-        layout.addWidget(Qt.QLabel("Some text"))
-        layout.addWidget(self.view)
-        layout.addWidget(self.btn)
-
-    def random_plot(self):
-        a = 60
+    def drawPoints(self, qp):
+        a = 45
         v = 10
-        random_array = []
-        for i in range(10):
-            random_array.append(
-                i * math.tan(math.radians(a)) - (9.8 * i ** 2) / (2 * v ** 2 * math.cos(math.radians(a)) ** 2))
-        self.curve.setData(random_array)
+
+        qp.setPen(Qt.black)
+        size = self.size()
+
+        for i in range(20):
+            x = i
+            y = i * math.tan(math.radians(a)) - (9.8 * i ** 2) / (2 * v ** 2 * math.cos(math.radians(a)) ** 2)
+            qp.drawPoint(280 - x * 10, 170 - y * 10)
 
 
-if __name__ == "__main__":
-    app = Qt.QApplication([])
-    w = Window()
-    w.show()
-    app.exec()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
