e0d5f4b83d5121cb5c47151ee70125de27ec09db
Rusik <rusgaliul00@gmail.com>
2018-12-09 15:09:42 +0300

    NEW

--- Povorot_pushki.py
+++ Povorot_pushki.py
@@ -1,20 +1,16 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, \
-    QPushButton, QLabel, QLineEdit, QLCDNumber, QHBoxLayout, QInputDialog
+    QPushButton, QLabel, QLineEdit, QLCDNumber, QHBoxLayout, QInputDialog, QMainWindow
 from PyQt5.QtGui import QPixmap, QTransform
 from PyQt5 import uic
 
 
-class Example(QWidget):
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         self.flag = True
-        self.initUI()
-
-    def initUI(self):
         uic.loadUi('project.ui', self)
-        if self.flag:
-            self.upravlenie()
+        self.nach_okno()
 
     def hello(self):
         try:
@@ -31,7 +27,7 @@ class Example(QWidget):
     def next(self, naklon):
         try:
             t = QTransform().rotate(self.angle - naklon)
-            self.label.setPixmap(self.pixmap.pixmap.transformed(t))
+            self.label.setPixmap(self.pixmap.transformed(t))
         except Exception as e:
             print(e)
 
@@ -39,17 +35,20 @@ class Example(QWidget):
         try:
             pixmap = QPixmap('w.png')
             self.label_1 = QLabel(self)
-            self.label_1.setPixmap(self.pixmap)
+            self.label_1.resize(pixmap.width(), pixmap.height())
+            self.label_1.setPixmap(pixmap)
             self.label_1.move(190, 170)
 
             pixmap_2 = QPixmap('s.png')
             self.label_2 = QLabel(self)
-            self.label_2.setPixmap(self.pixmap_2)
+            self.label_2.resize(pixmap_2.width(), pixmap_2.height())
+            self.label_2.setPixmap(pixmap_2)
             self.label_2.move(370, 170)
 
             pixmap_3 = QPixmap('space.png')
             self.label_3 = QLabel(self)
-            self.label_3.setPixmap(self.pixmap_3)
+            self.label_3.resize(pixmap_3.width(), pixmap_3.height())
+            self.label_3.setPixmap(pixmap_3)
             self.label_3.move(500, 170)
 
             self.show()
