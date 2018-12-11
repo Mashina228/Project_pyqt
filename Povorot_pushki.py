1596a6a747ad8e408ce35f386b2f0370d0155c99
Rusik <rusgaliul00@gmail.com>
2018-12-11 21:53:52 +0300

    True Rabotaet

--- Povorot_pushki.py
+++ Povorot_pushki.py
@@ -13,15 +13,13 @@ class Example(QWidget):
     def __init__(self):
         super().__init__()
         self.setGeometry(0, 0, 1920, 1080)
+        self.setWindowTitle('Traektoria')
 
-        oImage = QImage("fon.png")
-        sImage = oImage.scaled(QSize(self.width(), self.height()))  # resize Image to widgets size
-        palette = QPalette()
-        palette.setBrush(10, QBrush(sImage))  # 10 = Windowrole
-        self.setPalette(palette)
-
-        self.label = QLabel('название игры', self)  # test, if it's really backgroundimage
-        self.label.setGeometry(860, 500, 200, 50)
+        # oImage = QImage("fon.png")
+        # sImage = oImage.scaled(QSize(self.width(), self.height()))  # resize Image to widgets size
+        # palette = QPalette()
+        # palette.setBrush(10, QBrush(sImage))  # 10 = Windowrole
+        # self.setPalette(palette)
 
         self.pushButton = QPushButton('hdsghja', self)
         self.pushButton.setGeometry(860, 540, 100, 80)
@@ -29,10 +27,14 @@ class Example(QWidget):
         self.pushButton.clicked.connect(self.hello)
 
         # Блок настроек
+        self.flag = False
+        self.kon = True
+
+        # Блок настроек
         self.mish_x = 700  # расположение мишени
 
-        self.sdvig_x = 280  # Задаем отступ слева
-        self.sdvig_y = 755  # Задаем ширину поля
+        self.sdvig_x = 80  # Задаем отступ слева
+        self.sdvig_y = 725  # Задаем ширину поля
 
         self.a = 45  # Изначальный угол броска
         self.v = 113  # Начальная скорость
@@ -43,39 +45,34 @@ class Example(QWidget):
         self.label = QLabel(str(self.a), self)
         self.label.setGeometry(860, 500, 200, 50)
 
-        self.hp = QLabel('', self)
-        self.hp.setGeometry(880, 200, 200, 50)
-        self.run()
+        self.hp = QLabel('0' * self.kol_hp, self)
+        self.hp.setGeometry(880, 500, 200, 50)
 
-    def run(self):
-        i, okBtnPressed = QInputDialog.getInt(
-            self, "Давайте сыграем", "Сколько вы хотите жизней?", 3, 1, 3, 1
-        )
-        if okBtnPressed:
-            self.kol_hp = int(i)
-            self.hp.setText('0' * self.kol_hp)
-            self.show()
+        self.show()
 
     def keyPressEvent(self, event):
-        if event.key() == 1062:
+        if event.key() == Qt.Key_W or event.key() == 1062:
             if self.a <= 80:
+                self.povorot_pushki(-5)
                 self.a += 5
                 self.label.setText(str(self.a))
                 self.flag = False
-        if event.key() == 1067:
+        if event.key() == Qt.Key_S or event.key() == 1067:
             if self.a >= 10:
+                self.povorot_pushki(5)
                 self.a -= 5
                 self.label.setText(str(self.a))
                 self.flag = False
-        if event.key() == 32:
+        if event.key() == Qt.Key_Space or event.key() == 32:
+            self.kon = True
             self.flag = True
 
     def paintEvent(self, e):
-        qp = QPainter()
-        qp.begin(self)
-        self.drawPoints(qp)
-        self.drawMishen(qp)
-        qp.end()
+        self.qp = QPainter()
+        self.qp.begin(self)
+        self.drawPoints(self.qp)
+        self.drawMishen(self.qp)
+        self.qp.end()
 
     def drawMishen(self, qp):  # Отрисовка мишени
         qp.setPen(Qt.black)
@@ -87,6 +84,11 @@ class Example(QWidget):
 
         for i in range(1920):  # Отрисовка пола
             qp.drawPoint(i, self.sdvig_y)
+
+        qp.setPen(Qt.white)
+        for i in range(self.sdvig_y + 1, 1081):
+            for j in range(0, 1920):
+                qp.drawPoint(j, i)
 
         if self.flag:
             qp.setPen(Qt.red)
@@ -107,18 +109,15 @@ class Example(QWidget):
                 y = k1
                 qp.drawPoint(x + self.sdvig_x, self.sdvig_y - y)
 
-                if k2 == 0:
-                    self.vivod(False)
-                    break
-
                 if x + self.sdvig_x in range(self.mish_x, self.mish_x + 21) and self.sdvig_y - y in range(
                         self.sdvig_y - 120, self.sdvig_y + 1):
-                    self.vivod(True)
+                    if self.kon:
+                        self.vivod(True)
                     break
-            qp.setPen(Qt.white)
-            for i in range(self.sdvig_y + 1, 1081):
-                for j in range(0, 1920):
-                    qp.drawPoint(j, i)
+
+                if k2 < 0:
+                    if self.kon:
+                        self.vivod(False)
 
     def vivod(self, ishod):
         if ishod:
@@ -131,28 +130,24 @@ class Example(QWidget):
             else:
                 self.kol_hp = self.poln_hp
                 self.hp.setText('You lose')
+        self.kon = False
 
     def cleaning_first(self):
-        self.label.setText('')
         self.pushButton.deleteLater()
 
     def hello(self):
-        try:
-            self.pic = QLabel(self)
-            self.pixmap = QPixmap('pushka3.0.png')
-            self.pic.setPixmap(self.pixmap)
-            self.pic.move(280, 755)
-            self.pic.show()
-        except Exception as e:
-            print(e)
+        self.angel = -45
+        self.pic = QLabel(self)
+        self.pixmap = QPixmap('pushka3.0.png')
+        t = QTransform().rotate(self.angel)
+        self.pic.setPixmap(self.pixmap.transformed(t))
+        self.pic.move(-1, self.sdvig_y - 128)
+        self.pic.show()
 
-    def next(self, naklon):
-        try:
-            t = QTransform().rotate(45 + naklon)
-            self.pic.setPixmap(self.pixmap.transformed(t))
-            self.hbox.addWidget(self.pic)
-        except Exception as e:
-            print(e)
+    def povorot_pushki(self, naklon):
+        self.angel += naklon
+        t = QTransform().rotate(self.angel)
+        self.pic.setPixmap(self.pixmap.transformed(t))
 
 
 if __name__ == '__main__':
