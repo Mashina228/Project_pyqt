7311834c44f13205351fe2bd39f9cf4f9e88b6ba
fatahov <fatahoff.georgy@yandex.ru>
2018-12-11 21:17:11 +0300

    ВСЕ ОТЛИЧНО РАБОТАЕТ Еще лучше и пол+

--- points_old.py
+++ points_old.py
@@ -3,7 +3,6 @@ from PyQt5.QtWidgets import QWidget, QApplication, QLabel
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtCore import Qt
 import math
-import time
 
 
 class Example(QWidget):
@@ -91,7 +90,6 @@ class Example(QWidget):
                 y = k1
                 qp.drawPoint(x + self.sdvig_x, self.sdvig_y - y)
 
-                #print(x + self.sdvig_x, self.sdvig_y - y)
                 if x + self.sdvig_x in range(self.mish_x, self.mish_x + 21) and self.sdvig_y - y in range(
                         self.sdvig_y - 120, self.sdvig_y + 1):
                     if self.kon:
